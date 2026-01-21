75dc5bd2f60ab971631f6c055a36861dff28fdc2
Desing8Susana <88susa888@gmail.com>
2026-01-21 13:39:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function mostrarSeccion(id) { const secciones = document.querySelectorAll('.seccion'); secciones.forEach(sec => sec.classList.add('oculto')); document.getElementById(id).classList.remove('oculto'); }
 
+let permitido = false;
+
+// Desbloqueo inicial (OBLIGATORIO en móvil)
+document.addEventListener("click", desbloquearAudio, { once: true });
+document.addEventListener("touchstart", desbloquearAudio, { once: true });
+
+function desbloquearAudio() {
+  const u = new SpeechSynthesisUtterance("");
+  speechSynthesis.speak(u);
+  speechSynthesis.cancel();
+  permitido = true;
+}
 
 function hablar(texto) {
-	
-    const mensaje = new SpeechSynthesisUtterance(texto);
-    mensaje.lang = "es-ES";
-	mensaje.rate = 0.9
-	mensaje.pitch = 1
-	
-    window.speechSynthesis.speak(mensaje);
+  if (!permitido) return;
+
+  speechSynthesis.cancel();
+
+  const voz = new SpeechSynthesisUtterance(texto);
+  voz.lang = "es-ES";
+  voz.rate = 0.85;
+  voz.pitch = 1;
+
+  speechSynthesis.speak(voz);
 }
