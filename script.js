384f3bc3a1e21b1587a1a2240ad303a634e31542
Desing8Susana <88susa888@gmail.com>
2026-01-21 13:15:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function mostrarSeccion(id) { const secciones = document.querySelectorAll('.seccion'); secciones.forEach(sec => sec.classList.add('oculto')); document.getElementById(id).classList.remove('oculto'); }
+
+
 function hablar(texto) {
     const mensaje = new SpeechSynthesisUtterance(texto);
     mensaje.lang = "es-ES";
